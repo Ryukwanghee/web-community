07191993dd4b948b152d3a4d5ce22b75f7dfcbb8
hans-woo <seung94woo@naver.com>
2022-12-08 10:03:59 +0900

    file게시판 리스트 관련 파일 추가

--- src/main/java/com/community/vo/FileShare.java
+++ src/main/java/com/community/vo/FileShare.java
@@ -1,5 +1,30 @@
 package com.community.vo;
 
 public class FileShare {
+	
+	private int no;			// 파일번호
+	private String name;	// 파일이름
+	private int postNo;		// 게시글번호
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPostNo() {
+		return postNo;
+	}
+	public void setPostNo(int postNo) {
+		this.postNo = postNo;
+	}
+	
+	
 
 }
